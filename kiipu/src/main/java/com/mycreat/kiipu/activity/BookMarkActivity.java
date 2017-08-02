0c22b103f9fa78236081deb2d21395ebc997c076
leaderliang <leaderliang.dev@gmail.com>
2017-08-02 15:33:42 +0800

    delete

--- kiipu/src/main/java/com/mycreat/kiipu/activity/BookMarkActivity.java
+++ kiipu/src/main/java/com/mycreat/kiipu/activity/BookMarkActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -113,8 +114,6 @@ public class BookMarkActivity extends BaseActivity
     private int mScrollThreshold;
 
     private Button mBtLogOut;
-
-    private boolean isDownloading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
